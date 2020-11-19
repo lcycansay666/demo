c21ff97533667ca9685b6861ea479639a3b02510
lcycansay666 <ainidw1314>
2020-11-19 16:07:14 +0800

    测试提交

---
+++ src/main/java/com/example/entity/Animal.java
@@ -0,0 +1,24 @@
+/**
+ * @Auther: PC-8
+ * @Date: 2020/11/3 16:54
+ * @Description:
+ */
+package com.example.entity;
+
+/**
+ * @Author: lcy
+ * @Date: 2020/11/3 16:54
+ * @Description:
+ */
+public class Animal {
+
+    private  String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/entity/Dog.java
@@ -0,0 +1,25 @@
+/**
+ * @Auther: PC-8
+ * @Date: 2020/11/3 16:57
+ * @Description:
+ */
+package com.example.entity;
+
+/**
+ * @Author: lcy
+ * @Date: 2020/11/3 16:57
+ * @Description:
+ */
+public class Dog extends Animal{
+
+
+    private Integer leg;
+
+    public Integer getLeg() {
+        return leg;
+    }
+
+    public void setLeg(Integer leg) {
+        this.leg = leg;
+    }
+}
